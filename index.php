e0f9d73fc183fa849dc62dc263de367ac8641be8
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-11-01 18:38:11 -0500

    shareMenu, validated**
    added share menu, social media label and fixed css drop down menus

--- index.php
+++ index.php
@@ -1,54 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8"/>
+<head>
+	<meta charset="utf-8"/>
 
-		<title>Peekskill Film Festival</title>
-		<link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-		<link href="css/main.css" rel="stylesheet" type="text/css">
-	</head>
-	<body>
-		<div class="header">
-			<section id="social">
-				<a href="https://www.facebook.com/peekskillfilmfest" target="_blank"><i class="fa fa-facebook"></i></a>
-                		<a href="https://twitter.com/PKfilmfest" target="_blank"><i class="fa fa-twitter"></i></a>
-                		<a href="https://instagram.com/peekskillfilmfest/" target="_blank"><i class="fa fa-instagram"></i></a>
-			</section>
-			<nav>
-				<ul id="nav">
-					<li id="about"> <a href="#"> </a>
-						<ul class="sub">
-							<li>Films</li>
-							<li>Festival</li>
-							<li>Donate</li>
-							<li>Become a Sponsor</li>
-						</ul>
-					</li>
-					<li id="schedule"><a href="#"></a>
-						<ul class="sub">
-                        	<li>View Online</li>
-                        	<li>Print</li>
-                        </ul>
-					</li>
-					<li id="updates"><a href="#"></a></li>
-					<li id="contact"><a href="#"></a>
-						<ul class="sub">
-                        	<li>Donate</li>
-                            <li>Become a Sponsor</li>
-                            <li>General Inquiries</li>
-                            <li>Email</li>
-                        </ul>
-					</li>
-					<li id="submit"><a href="#"></a>
-						<ul class="sub">
-							<li>Submit your Indie Film</li>
-						</ul>
-					</li>
+	<title>Peekskill Film Festival</title>
+	<link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link href="css/main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="header">
+	<section id="social">
+		<label>Get in touch: </label><br>
+		<a href="https://www.facebook.com/peekskillfilmfest" target="_blank"><i class="fa fa-facebook"></i></a>
+		<a href="https://twitter.com/PKfilmfest" target="_blank"><i class="fa fa-twitter"></i></a>
+		<a href="https://instagram.com/peekskillfilmfest/" target="_blank"><i class="fa fa-instagram"></i></a>
+	</section>
+	<nav>
+		<ul id="nav">
+			<li id="about"> <a href="#"> </a>
+				<ul class="sub">
+					<li>Films</li>
+					<li>Festival</li>
+					<li>Donate</li>
+					<li>Become a Sponsor</li>
 				</ul>
-			</nav>
-		</div>
-		<h1>This is the body</h1>
-		<p>This is some text.</p>
-	</body>
+			</li>
+			<li id="schedule"><a href="#"></a>
+				<ul class="sub">
+					<li>View Online</li>
+					<li>Print</li>
+				</ul>
+			</li>
+			<li id="updates"><a href="#"></a></li>
+			<li id="contact"><a href="#"></a>
+				<ul class="sub">
+					<li>Donate</li>
+					<li>Become a Sponsor</li>
+					<li>General Inquiries</li>
+					<li>Email</li>
+				</ul>
+			</li>
+			<li id="submit"><a href="#"></a>
+				<ul class="sub">
+					<li>Submit your Indie Film</li>
+				</ul>
+			</li>
+			<li id="share"><a href="#"></a>
+				<ul class="sub">
+					<li><i class="fa fa-facebook"></i> Facebook</li>
+					<li><i class="fa fa-twitter"></i> Twitter</li>
+					<li><i class="fa fa-google-plus"></i> Google+</li>
+					<li><i class="fa fa-instagram"></i> Instagram</li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+</div>
+<h1>This is the body</h1>
+<p>This is some text.</p>
+</body>
 </html>
