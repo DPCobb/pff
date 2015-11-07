82e35beb815d8ad7b97519f2793aa16fae02ad34
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-11-07 18:52:34 -0500

    filled in info
    filling in sched info

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 
 	<title>Peekskill Film Festival</title>
 	<link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Abril+Fatface' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link href="css/main.css" rel="stylesheet" type="text/css">
 </head>
@@ -59,17 +60,52 @@
 </div>
 <div id="container">
 	<section id="showinfo">
+		<h1>Show Dates and Times</h1>
+		<h2>Friday May 13th</h2>
+
+		<p>6:00pm – Red Carpet reception</p>
+
+		<p>7:00pm – Film Screening – “But not for me”</p>
+
+		<p>9:30pm – Live Q&A w/ Directors</p>
+
+		<p>10pm – Original Film Score Performance</p>
+
+		<h2>Saturday May 14th</h2>
+
+
+		<p>12:00-2pm – Film Workshop & Networking – Impolite Production Company</p>
+
+		<p>3pm-5pm – Independent Film Festival – Curated by Point of Order Productions</p>
+
+		<p>6pm – Dinner Break</p>
+
+		<p>7pm – Red Carpet reception</p>
+
+		<p>8pm – “La Jaula de Oro” – “The Golden Dream” Screening</p>
+
+		<p>9:30pm – Latin Music Performance</p>
 
 	</section>
-	<section id="wrapper">
+	<div id="wrapper">
 		<section id="events">
+			<h1>Events and Workshops</h1>
+			<h2>Friday May 13th</h2>
+			<p>9:30pm – Live Q&A w/ Directors of "But Not For Me"</p>
+			<h2>Saturday May 14th</h2>
+			<p>12:00-2pm – Film Workshop & Networking – Impolite Production Company</p>
+
 
 		</section>
 		<section id="eventupdates">
-
+			<h1>Updates</h1>
+			<a class="twitter-timeline" href="https://twitter.com/PKfilmfest" data-widget-id="663134995448467456" data-chrome="transparent nofooter noborders noheader noborders" data-tweet-limit="2">Tweets by @PKfilmfest</a>
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 		</section>
-	</section>
+	</div>
 </div>
+<footer>
 
+</footer>
 </body>
 </html>
